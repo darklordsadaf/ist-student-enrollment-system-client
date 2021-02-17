3e18bb43af5842986ce443a3d9f919968970e0a1
Sadaf Rahman <darklordsadaf@gmail.com>
2021-02-18 05:56:56 +0600

    unauthorized updated

--- src/components/NotAccess/Unauthorized/Unauthorized.js
+++ src/components/NotAccess/Unauthorized/Unauthorized.js
@@ -9,15 +9,6 @@ const Unauthorized = () => {
     const [loggedInUser, setLoggedInUser] = useContext(UserContext);
 
 
-    const logout = () => {
-        localStorage.clear();
-        setLoggedInUser({})
-    }
-    // function MyComponent3() {
-    //     useEffect(() => {
-    //         setIsAdmin(JSON.parse(localStorage.getItem("admin")) || {});
-    //     }, [])
-    // }
     function MyComponent3() {
         useEffect(() => {
             setIsAdmin(JSON.parse(localStorage.getItem("admin")) || {});
@@ -43,26 +34,20 @@ const Unauthorized = () => {
     else {
         MyComponent4();
     }
-    // function MyComponent4() {
 
-    // }
-    // if (localStorage.getItem("admin")) {
-    //     MyComponent3();
-    // }
-    // else {
-    //     MyComponent4();
-    // }
     return (
         <div >
 
             {
-                isAdmin === false ? <div >
-                    {/* <div className="text-right mt-2"><Link to='/' style={{ background: '#111430', borderRadius: '5px' }} className="" className="btn mr-4 text-white " onClick={logout}>Log Out</Link></div> */}
-                    <Navbar />
-                    <h1 style={{ fontSize: '200px', marginTop: '70px', color: 'red' }} class="text-center " >Access Denied</h1>
-                    <br />
-                    {/* <div className="text-center mt-3"><Link style={{ fontSize: '50px', borderRadius: '50px' }} className=" btn btn-warning" to="/">Go to Home</Link></div> */}
-                </div> : <img style={{ width: '500px' }} className="rounded mx-auto d-block mt-4 pt-5" src="https://media1.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif" alt="" />
+                isAdmin === true ? <img style={{ width: '500px' }} className="rounded mx-auto d-block mt-4 pt-5" src="https://media1.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif" alt="" />
+
+                    : <div >
+
+                        <Navbar />
+                        <h1 style={{ fontSize: '200px', marginTop: '70px', color: 'red' }} class="text-center " >Access Denied</h1>
+                        <br />
+
+                    </div>
             }
         </div>
     );
